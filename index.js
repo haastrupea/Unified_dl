7a43b392fc71a67751a768c67296c28ef9018bea
Haastrup Elijah <haastrupea@gmail.com>
2019-07-29 10:57:53 +0100

    Call python process through node child_process module

--- index.js
+++ index.js
@@ -14,3 +14,28 @@ using child_process module
 --user can see download process of course[s] or its file[s]
 --user can preview specific downloaded file in a course or play the whole downloaded courses via vlc just by pushing play button from unified downloader.
 */
+
+const spawn=require("child_process").spawn;
+var   you=false;
+const cp=spawn("pip",["install","youtube-dl"]);
+        cp.stdout.on("data",d=>{
+            console.log(d.toString())
+           you = d.toString().toLowerCase().indexOf("satisfied")!==-1?true:false;
+        })
+
+    cp.on("close",(err,sig)=>{
+        if(you===true){
+            console.log(you);
+        }
+        if(sig){
+
+            cp.kill(sig)
+        }
+    })
+    
+    
+    process.stdin.on('data',d=>{
+        console.log(d.toString());  
+        process.stdin.write("hello")
+    })
+    
